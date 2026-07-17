c0200c30a57c8283eee2c9b9e56e4b1cea7bbf46
Tsavargo <gamekapocshu@gmail.com>
2026-07-17 22:44:10 +0200

    Move calude.fish

--- config/fish/.config/fish/conf.d/claude.fish
+++
@@ -1,9 +0,0 @@
-set -gx ANTHROPIC_BASE_URL "https://api.deepseek.com/anthropic"
-
-set -gx ANTHROPIC_MODEL "deepseek-v4-pro"
-set -gx ANTHROPIC_DEFAULT_OPUS_MODEL "deepseek-v4-pro"
-set -gx ANTHROPIC_DEFAULT_SONNET_MODEL "deepseek-v4-pro"
-set -gx ANTHROPIC_DEFAULT_HAIKU_MODEL "deepseek-v4-flash"
-set -gx CLAUDE_CODE_SUBAGENT_MODEL "deepseek-v4-flash"
-
-set -gx CLAUDE_CODE_EFFORT_LEVEL "max"
