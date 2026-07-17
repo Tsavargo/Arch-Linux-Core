13d21d8563580630b0b23dc70568c03c145f5d8f
Tsavargo <gamekapocshu@gmail.com>
2026-07-18 01:34:42 +0200

    Update ghostty installer: description and comments

--- installers/ghostty.sh
+++ installers/ghostty.sh
@@ -1,3 +1,10 @@
+# [PACKAGE] Ghostty
+# Fast, feature-rich terminal emulator
+#
+# https://ghostty.org/
+
 sudo pacman -S --noconfirm --needed ghostty
+
+# symlink ghostty config into place via GNU stow
 cd "$HOME/system/config"
 stow -t "$HOME" ghostty
