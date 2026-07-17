9e6faf1e5404e0a69f0d75b4dbcc8d65f18f3c80
Tsavargo <gamekapocshu@gmail.com>
2026-07-17 18:39:00 +0200

    Update fish installer: - Add package description and comments

--- installers/fish.sh
+++ installers/fish.sh
@@ -1,4 +1,14 @@
+# [PACKAGE] Fish
+# Smart and user-friendly command line shell
+#
+# https://fishshell.com/
+# https://wiki.archlinux.org/title/Fish
+
 sudo pacman -S --noconfirm --needed fish
-chsh -s "$(which fish)"
+
+# change the default shell to fish
+chsh -s "$(command -v fish)"
+
+# symlink fish config into place via GNU stow
 cd "$HOME/system/config"
 stow -t "$HOME" fish
