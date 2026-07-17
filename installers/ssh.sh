b3f045f7144bb9fd9658728b2946d1121646e222
Tsavargo <gamekapocshu@gmail.com>
2026-07-17 18:36:21 +0200

    Update OpenSSH installer - Merge systemctl enable and start - Add firewall rules for port 22/tcp and limit

--- installers/ssh.sh
+++ installers/ssh.sh
@@ -1,3 +1,14 @@
+# [PACKAGE] OpenSSH
+# Secure remote login and file transfer
+#
+# https://www.openssh.org/
+# https://wiki.archlinux.org/title/OpenSSH
+
 sudo pacman -S --noconfirm --needed openssh
-sudo systemctl enable sshd
-sudo systemctl start sshd
+# enable and start sshd as a system service
+sudo systemctl enable --now sshd
+
+# allow ssh (port 22 on tcp) connection on firewall (ufw)
+sudo ufw allow ssh
+sudo ufw limit ssh
+sudo ufw enable
