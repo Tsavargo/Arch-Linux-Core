7edac5e039481b67d164c80f7ad3017e2ff0b879
Tsavargo <gamekapocshu@gmail.com>
2026-07-18 01:37:33 +0200

    Update mediaplayer.sh: description

--- installers/mediaplayer.sh
+++ installers/mediaplayer.sh
@@ -1,4 +1,9 @@
+# [PACKAGE] VLC Media Player
+# Free and open source multimedia player
+#
+# https://wiki.archlinux.org/title/VLC_media_player
+
 sudo pacman -S --noconfirm --needed \
-vlc \
-vlc-plugins-base \
-vlc-plugins-extra \
+	vlc \
+	vlc-plugins-base \
+	vlc-plugins-extra \
